440ec8a20216c1831584a1d226b8dc1ce06a2c74
Sebastian Guevara <sg988170@gmail.com>
2024-07-03 20:08:06 +0000

    add py anywhere

--- webApp.py
+++ webApp.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for, flash, redirect
+import git
+from flask import Flask, render_template, url_for, flash, redirect, request
 from forms import RegistrationForm
 from flask_behind_proxy import FlaskBehindProxy
 
@@ -12,6 +13,16 @@ app.config['SECRET_KEY'] = 'your_secret_key_here'
 @app.route("/home")
 def home():
     return render_template('home.html', subtitle='Home Page', text='This is the home page')
+
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/sebguev/Week3Page')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
